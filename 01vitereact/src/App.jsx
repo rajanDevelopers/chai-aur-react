cd0b41e73b4179caed2acce09005c791ede5efa1
RAJAN KUMAR MAHTO <you@example.com>
2023-12-05 23:23:39 +0530

    First day React

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+ 
+  return (
+
+    <h1>Chai And React with Vite | Today i learnd how to create the Small App using React Librery and how to Clean the Code under of the src File </h1> 
+   
+    
+   
+  );
+}
+
+export default App;
---
+++ redme.md
@@ -0,0 +1,8 @@
+Test
+
+// how to push the code in GitHub
+A Code repo for Vite React at chai and Code
+
+==> git add -A
+==> git commit -m "Chai and Code"
+==> git push
