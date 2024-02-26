76d928215a0f84733c4b3d1e34bb727b85b06bce
RAJAN KUMAR MAHTO <you@example.com>
2024-02-26 23:29:31 +0530

    bgchanger

--- database/ifelse.sql
+++ database/ifelse.sql
@@ -9,6 +9,7 @@
                 CASE    
                     WHEN A = B AND B = C  THEN 'Equilateral'
                     WHEN A = B  OR B = C OR A = C THEN 'Isosceles'
+        
                     ELSE 'Scalene'       
                 END 
             ELSE 'Not A Triangle'
